e413107ea31e2f29597435ed38c9fa2b551cc207
Fernando Favini <darthjee@gmail.com>
2017-03-17 23:55:21 -0300

    Add for_month

--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -8,7 +8,26 @@ describe Bill do
       end
 
       it 'returns only active' do
-        expect(described_class.uniq.pluck(:active)).to eq([true])
+        expect(described_class.distinct.pluck(:active)).to eq([true])
+      end
+    end
+
+    describe '.for_month' do
+      before do
+        Timecop.freeze(2017,03,10)
+      end
+      let(:start_date) { Date.new(2017,03,01) }
+      let(:end_date) { Date.new(2017,03,31) }
+      let(:first_start_date) do
+        described_class.for_month(Date.today).order(:start_date).distinct.pluck(:start_date).first
+      end
+      let(:last_end_date) do
+        described_class.for_month(Date.today).order(:end_date).distinct.pluck(:end_date).last
+      end
+
+      it 'returns only the bills for this month' do
+        expect(first_start_date).to be >= start_date
+        expect(last_end_date).to be <= end_date
       end
     end
   end
