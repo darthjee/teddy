1b4b7e59e9dcc70dcef049b23a0607503a8f30c3
Fernando Favini <darthjee@gmail.com>
2017-03-22 13:07:44 -0300

    Add preliminary spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:subject) { users(:default) }
+
+  describe '#bills' do
+    it { expect(subject).to respond_to(:bills) }
+  end
 end
