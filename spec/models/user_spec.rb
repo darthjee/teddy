3e6d8d9e8dda32c32b491a85617f3f2450d75a0c
Fernando Favini <darthjee@gmail.com>
2017-03-26 17:26:06 -0300

    Ensure user has email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe User do
   let(:subject) { users(:default) }
 
+  describe 'validations' do
+    it { expect(subject).to validate_presence_of(:email) }
+  end
+
   describe '#bills' do
     it { expect(subject).to respond_to(:bills) }
     it { expect(subject.bills).not_to be_empty }
