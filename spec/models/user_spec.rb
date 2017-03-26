14c58800cf7ffd5079e0027a6b5eab368edce9cc
Fernando Favini <darthjee@gmail.com>
2017-03-26 17:51:49 -0300

    Add user factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe User do
-  let(:subject) { users(:default) }
+  let(:subject) { build :user }
 
   describe 'validations' do
     it { expect(subject).to validate_presence_of(:email) }
   end
 
   describe '#bills' do
+    let(:subject) { users(:default) }
+
     it { expect(subject).to respond_to(:bills) }
     it { expect(subject.bills).not_to be_empty }
   end
