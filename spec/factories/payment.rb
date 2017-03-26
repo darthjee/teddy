73c4906fb9836602bedb3ed8a1f87c7a96b33298
Fernando Favini <darthjee@gmail.com>
2017-03-26 18:17:34 -0300

    Add payment factory

---
+++ spec/factories/payment.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :payment do
+    due_date Date.today
+    bill
+  end
+end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -2,14 +2,7 @@ require 'spec_helper'
 
 describe Payment do
   describe 'validations' do
-    let(:bill) { bills(:active) }
-    let(:attributes) do
-      {
-        due_date: Date.today,
-        bill: bill
-      }
-    end
-    let(:subject) { described_class.new(attributes) }
+    let(:subject) { build :payment }
 
     it { expect(subject).to validate_presence_of(:bill) }
     it { expect(subject).to validate_presence_of(:due_date) }
