bce27a0bba51f5d4051eb16a2aa1115c960a1b91
Fernando Favini <darthjee@gmail.com>
2017-03-27 00:15:14 -0300

    Define libs to be used

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require underscore
+//= require angular/angular
+//= require angular-route/angular-route
+//= require querystring
+//= require md5
 //= require_tree .
