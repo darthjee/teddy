ac661b8f82ef960cb2d8306de6ae2a049e94ad35
Fernando Favini <darthjee@gmail.com>
2017-04-02 13:42:36 -0300

    Generic Controller now performs the generic request

--- app/assets/javascripts/controllers/global/generic_controller.js
+++ app/assets/javascripts/controllers/global/generic_controller.js
@@ -1,13 +1,28 @@
 (function(_, angular) {
-  function GenericController($routeParams) {
+  function GenericController($location, $http) {
+    this.path = $location.$$path + '.json';
+    this.http = $http;
+
+    _.bindAll(this, '_setData');
+
+    this.request();
   }
 
   var fn = GenericController.prototype,
       app = angular.module('global/generic_controller', [
       ]);
 
+  fn.request = function() {
+    var promise = this.http.get(this.path);
+    promise.then(this._setData);
+  };
+
+  fn._setData = function(config, data) {
+    this.data = data;
+  };
+
   app.controller('GenericController', [
-    '$routeParams', 
+    '$location','$http',
     GenericController
   ]);
 })(window._, window.angular);
