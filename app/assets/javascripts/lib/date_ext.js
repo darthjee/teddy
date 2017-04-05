cdca806db53322a409a22f08f8df0acd811667ae
Fernando Favini <darthjee@gmail.com>
2017-04-04 21:41:55 -0300

    Add addDays to JS Date

--- app/assets/javascripts/lib/date_ext.js
+++ app/assets/javascripts/lib/date_ext.js
@@ -1,9 +1,19 @@
-(function(fn, _, undefined) {
-  fn.fromString = function(string) {
+(function(clazz, _, undefined) {
+  clazz.fromString = function(string) {
     var [year, month, day] = _.map(string.split('-'), function(s) {
       return Number.parseInt(s);
     });
     return new Date(year, month-1, day);
+  };
+
+  var fn = clazz.prototype;
+
+  fn.addDays = function(days) {
+    var year = this.getFullYear(),
+        month = this.getMonth(),
+        day = this.getDate() + days;
+
+    return new Date(year, month, day);
   };
 })(Date, window._);
 
