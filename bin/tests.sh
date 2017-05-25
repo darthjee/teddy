0d4796e36562fb343baecdfbdea378ecd9db4cf5
darthjee <darthjee@gmail.com>
2017-05-25 18:07:59 +0200

    Do not doble install gems

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 apt-get install nodejs
-# ensure bundler is present
-gem install bundler
-
-# install dependencies and clean old ones at the same time
-bundle install --clean
 
 # make sure Postgres is available
 until nc -z postgres 5432; do echo Waiting for Posgres; sleep 1; done
