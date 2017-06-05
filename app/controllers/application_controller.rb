16a5a7a2be08abd392ea19e2f64a92cf81efc0b5
darthjee <darthjee@gmail.com>
2017-06-05 02:00:30 +0200

    Camelize outputted json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   def render_basic
     action = params[:action]
     respond_to do |format|
-      format.json { render json: send("#{action}_json") }
+      format.json do
+        render json: send("#{action}_json").change_keys(&:to_s).lower_camelize_keys
+      end
       format.html { render action }
     end
   end
