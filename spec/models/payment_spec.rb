58a731adcbdbc6d25ca437402fde8ba44fff0a72
darthjee <darthjee@gmail.com>
2017-06-05 17:56:37 +0200

    Add period scope

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -7,4 +7,32 @@ describe Payment do
     it { expect(subject).to validate_presence_of(:bill) }
     it { expect(subject).to validate_presence_of(:due_date) }
   end
+
+  describe 'scopes' do
+    let(:bill) { bills(:active) }
+    let(:payments) { bill.payments }
+
+    describe 'period' do
+      let(:target_date) { Date.today + 1.day }
+      before do
+        Timecop.freeze(2016,03,10) do
+          bill.create_payment
+        end
+        bill.create_payment(target_date)
+      end
+
+      context 'when requesting the target month' do
+        it do
+          expect(payments.period(target_date)).not_to be_empty
+        end
+
+        it do
+          expect(payments.period(target_date).count).to eq(1)
+        end
+      end
+
+      context 'when not passing the month' do
+      end
+    end
+  end
 end
