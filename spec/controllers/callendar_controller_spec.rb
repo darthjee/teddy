1547e1a399e8d9a486e2dbc06a4eed7799205a7e
darthjee <darthjee@gmail.com>
2017-06-25 20:06:27 +0200

    Spec with underscore

--- spec/controllers/callendar_controller_spec.rb
+++ spec/controllers/callendar_controller_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe CalendarController do
   describe 'GET index' do
     let(:parameters) { { year: 2017, month: 3, format: :json } }
-    let(:response_json) { JSON.parse(response.body).deep_symbolize_keys }
+    let(:response_json) { JSON.parse(response.body).underscore_keys.deep_symbolize_keys }
 
     context 'when requesting for a month that has bills' do
       it 'returns the end and start of month' do
         get :index, params: parameters
 
         expect(response_json).to include(
-          firstDate: 1488326400000,
-          lastDate: 1490918400000
+          first_date: 1488326400000,
+          last_date: 1490918400000
         )
       end
     end
