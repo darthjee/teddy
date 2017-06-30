1d51c49377bd6de56634704d8cfc13e87db65085
darthjee <darthjee@gmail.com>
2017-06-30 18:51:32 +0200

    Add spec to avoid double payment creation

--- spec/controllers/callendar_controller_spec.rb
+++ spec/controllers/callendar_controller_spec.rb
@@ -6,6 +6,8 @@ describe CalendarController do
     let(:year) { 2017 }
     let(:parameters) { { year: year, month: month, format: :json } }
     let(:response_json) { JSON.parse(response.body).underscore_keys.deep_symbolize_keys }
+    let(:previews_month) { Date.new(year, month - 1, 1) }
+    let(:current_month) { Date.new(year, month, 1) }
 
     context 'when requesting for a month that has bills' do
       let(:payments_json) { response_json[:payments] }
@@ -30,8 +32,33 @@ describe CalendarController do
           ])
         end
 
+        it 'should create new payments' do
+          expect do
+            get :index, params: parameters
+          end.to change(Payment, :count).by(1)
+        end
+
+        context 'when there is already payment for this month' do
+          before do
+            Bill.all.each { |b| b.create_payment(current_month) }
+          end
+
+          it 'returns the payments for this month' do
+            get :index, params: parameters
+
+            expect(payments_json).to match([
+              hash_including(due_date:1489104000000, bill_id: 1, paid: nil)
+            ])
+          end
+
+          it 'should not create new payments' do
+            expect do
+              get :index, params: parameters
+            end.not_to change(Payment, :count)
+          end
+        end
+
         context 'when there is a payment for the previews month' do
-          let(:previews_month) { Date.new(year, month - 1, 1) }
           before do
             Bill.all.each { |b| b.create_payment(previews_month) }
           end
@@ -44,7 +71,7 @@ describe CalendarController do
             )
           end
 
-          it do
+          it 'does not return the payment for previows month' do
             get :index, params: parameters
             expect(payments_json).not_to include(
               hash_including(due_date: 1486684800000, bill_id: 1, paid: nil)
