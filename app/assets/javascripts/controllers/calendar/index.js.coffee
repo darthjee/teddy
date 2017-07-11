90c846d2b1ca2b5ddf06d558c44912914e000632
darthjee <darthjee@gmail.com>
2017-07-11 23:02:25 +0200

    Move calendar logic to model

--- app/assets/javascripts/controllers/calendar/index.js.coffee
+++ app/assets/javascripts/controllers/calendar/index.js.coffee
@@ -9,36 +9,11 @@ class Calendar.IndexController extends Global.GenericController
 
   _setData: (response) =>
     this.calendar = new Calendar.Calendar(response.data)
-    this.firstDate = new Date(response.data.firstDate)
-    this.lastDate = new Date(response.data.lastDate)
-    this.beginningOfWeek = this.firstDate.beginningOfWeek()
-    this.payments = response.data.payments
-    this.weeks = this._calculateWeeks()
-
-    this._buildCalendar()
 
   _buildWeekHeader: ->
     base = new Date().beginningOfWeek()
     this.weekHeader = _.times(7, (days) ->
       base.addDays(days).toLocaleFormat('%a')
-    )
-
-  _calculateWeeks: () ->
-    start = this.beginningOfWeek
-    end = this.lastDate.endOfWeek().addDays(1)
-    start.weeksUntil(end)
-
-  _buildCalendar: () ->
-    this.calendar = _.times(this.weeks, this._buildWeek)
-
-  _buildWeek: (week) =>
-    firstDay = this.beginningOfWeek
-
-    _.times(7, (weekDay) ->
-      date = firstDay.addDays(week * 7 + weekDay)
-      {
-        day: date.getDate()
-      }
     )
 
 app.controller('Calendar.IndexController', [
--- app/assets/javascripts/models/calendar.js
+++ app/assets/javascripts/models/calendar.js
@@ -1,10 +1,42 @@
 (function(_, angular, Calendar) {
-  console.info(Calendar);
   module = angular.module('calendar/calendar', []);
   
   Calendar.Calendar = function(calendar) {
     _.extend(this, calendar);
+
+    this.firstDate = new Date(calendar.firstDate);
+    this.lastDate = new Date(calendar.lastDate);
+    this.beginningOfWeek = this.firstDate.beginningOfWeek();
+    this.payments = calendar.payments;
+    this.weeks = this._calculateWeeks();
+
+    _.bindAll(this, '_buildWeek');
+
+    this._buildCalendar();
   }
+
+  var fn = Calendar.Calendar.prototype;
+
+  fn._calculateWeeks = function() {
+    start = this.beginningOfWeek;
+    end = this.lastDate.endOfWeek().addDays(1);
+    return start.weeksUntil(end);
+  };
+
+  fn._buildCalendar = function() {
+    this.calendar = _.times(this.weeks, this._buildWeek);
+  };
+
+  fn._buildWeek = function(week) {
+    firstDay = this.beginningOfWeek;
+
+    return _.times(7, function(weekDay) {
+      date = firstDay.addDays(week * 7 + weekDay)
+      return {
+        day: date.getDate()
+      }
+    });
+  };
   
   Calendar.Calendar.Factory = function() {
     return new Calendar.Calendar();
