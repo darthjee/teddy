8bafd89b5d38ab213d0676db2147ed860807494d
darthjee <darthjee@gmail.com>
2017-07-12 10:55:31 +0200

    Wrap splat operator

--- app/models/serializers/bill.rb
+++ app/models/serializers/bill.rb
@@ -1,4 +1,4 @@
 class Serializers::Bill < ActiveModel::Serializer
-  attributes *(::Bill.attribute_names.map(&:to_sym))
+  attributes(*(::Bill.attribute_names.map(&:to_sym)))
 end
 
--- app/models/serializers/payment.rb
+++ app/models/serializers/payment.rb
@@ -1,4 +1,4 @@
 class Serializers::Payment < ActiveModel::Serializer
-  attributes *(::Payment.attribute_names.map(&:to_sym))
+  attributes(*(::Payment.attribute_names.map(&:to_sym)))
   has_one :bill, serializer: Serializers::Bill
 end
