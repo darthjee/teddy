496e0ac54db49ae37114e4a2c182b518be48b871
darthjee <darthjee@gmail.com>
2017-07-14 23:56:52 +0200

    Recode notifier

---
+++ app/assets/javascripts/services/global/notifier.js
@@ -0,0 +1,32 @@
+(function(_, angular, Global) {
+  module = angular.module('global/notifier', []);
+
+  Global.NotifierServiceFactory = function() {
+    return new NotifierService();
+  };
+
+  function NotifierService() {
+    this.watchs = {};
+  }
+
+  var fn = NotifierService.prototype;
+
+  fn.notify = function(key, value) {
+    _.each(this.listeners(key), function(callback) {
+      callback(value);
+    })
+  };
+
+  fn.register = function(key, callback) {
+    this.listeners(key).push(callback)
+  };
+
+  fn.listeners = function(key) {
+    if (this.watchs[key] == undefined) {
+      this.watchs[key] = [];
+    }
+    return this.watchs[key];
+  };
+
+module.service('notifier', [Global.NotifierServiceFactory])
+}(window._, window.angular, window.Global));
