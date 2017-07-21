0a050d90cdfa35a72fc1fd63e7356f21cd631a2e
darthjee <darthjee@gmail.com>
2017-07-21 18:47:57 +0200

    Use single test file

--- bin/tests.sh
+++ bin/tests.sh
@@ -2,6 +2,41 @@
 
 # make sure Postgres is available
 until nc -z postgres 5432; do echo Waiting for Posgres; sleep 1; done
-
 bundle exec rake db:drop db:create db:schema:load db:migrate
-bundle exec rspec -fd
+
+ARG=$1
+
+if [ ! $ARG ]; then
+  ARG=all
+fi
+
+function do_ci() {
+  bundle exec rake jasmine:ci
+}
+
+function do_rspec() {
+  bundle exec rspec -fd
+}
+
+function do_jasmine() {
+  bundle exec rake jasmine
+}
+
+case $ARG in
+  all)
+    do_rspec
+    do_ci
+    ;;
+  rspec)
+    do_rspec
+    ;;
+  ci)
+    do_ci
+    ;;
+  jasmine)
+    do_jasmine
+    ;;
+  *)
+    echo 'Usage: $0 <ci|rspec|jasmine>'
+    ;;
+esac
