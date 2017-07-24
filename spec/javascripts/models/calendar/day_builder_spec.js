282a2aef58b941ba839f5a4d8632204707bceeb8
darthjee <darthjee@gmail.com>
2017-07-24 15:26:27 +0200

    Use user scope aproach

--- spec/javascripts/models/calendar/day_builder_spec.js
+++ spec/javascripts/models/calendar/day_builder_spec.js
@@ -1,24 +1,20 @@
 describe("Calendar.DayBuilder", function() {
-  enhance(this)
-
-  var describedClass,
-      subject, firstDay;
-
-  this.let('firstDay', function() {
-    return new Date(2017, 05, 03);
-  });
-
-
-  firstDay = this.values().firstDay;
   beforeEach(function() {
-    describedClass = Calendar.DayBuilder;
-    subject = new describedClass(firstDay);
+    this.describedClass = Calendar.DayBuilder;
+    this.firstDay = new Date(2017, 05, 03);
+
+    this.buildSubject = function() {
+      return new this.describedClass(this.firstDay);
+    }
   });
 
   describe('#build', function() {
     describe('when passing 0 days as argument', function() {
       it('returns the first day from construction', function() {
-        expect(subject.build(0)).toEqual({ day: firstDay.getDate() });
+        with(this) {
+          var subject = this.buildSubject();
+          expect(subject.build(0)).toEqual({ day: firstDay.getDate() });
+        }
       });
     });
   });
