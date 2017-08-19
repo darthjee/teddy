8cfa3f0609a5b6f8f9d00086acac8d46ea6e6ba2
darthjee <darthjee@gmail.com>
2017-08-19 19:20:37 +0200

    Allow for custom postgres port

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 # make sure Postgres is available
-until nc -z postgres 5432; do echo Waiting for Posgres; sleep 1; done
-bundle exec rake db:drop db:create db:schema:load db:migrate
+until nc -z postgres $TEDDY_POSTGRES_PORT; do echo Waiting for Posgres; sleep 1; done
 
 ARG=$1
 
 if [ ! $ARG ]; then
   ARG=all
 fi
+
+function do_db() {
+  bundle exec rake db:drop db:create db:schema:load db:migrate
+}
 
 function do_ci() {
   bundle exec rake jasmine:ci
@@ -24,13 +27,18 @@ function do_jasmine() {
 
 case $ARG in
   all)
+    echo all
+    do_db
     do_rspec
     do_ci
     ;;
   rspec)
+    echo rspec
+    do_db
     do_rspec
     ;;
   ci)
+    echo ci
     do_ci
     ;;
   jasmine)
