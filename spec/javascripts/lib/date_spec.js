ef1bca0072a22be7a782f507bde74c378f2822b4
darthjee <darthjee@gmail.com>
2017-08-27 22:41:14 +0200

    Add spec for addDays

--- spec/javascripts/lib/date_spec.js
+++ spec/javascripts/lib/date_spec.js
@@ -16,4 +16,34 @@ describe('Date', function() {
       });
     });
   });
+
+  describe('#addDays', function() {
+    beforeEach(function() {
+      this.subject = new Date(2017, 10, 4);
+    });
+
+    describe('when adding 0 days', function() {
+      it('returns the same date', function() {
+        expect(this.subject.addDays(0)).toEqual(this.subject);
+      });
+    });
+
+    describe('when adding 1 days', function() {
+      it('returns the next day', function() {
+        expect(this.subject.addDays(1)).toEqual(new Date(2017, 10, 5));
+      });
+    });
+
+    describe('when adding -1 days', function() {
+      it('returns the previous day', function() {
+        expect(this.subject.addDays(-1)).toEqual(new Date(2017, 10, 3));
+      });
+    });
+
+    describe('when adding a whole month', function() {
+      it('returns the same day on next month', function() {
+        expect(this.subject.addDays(30)).toEqual(new Date(2017, 11, 4));
+      });
+    });
+  });
 });
