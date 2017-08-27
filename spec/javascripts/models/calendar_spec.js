6adb018f03aa6359b86eafc11734ced1b3f9e22e
darthjee <darthjee@gmail.com>
2017-08-27 15:52:04 +0200

    Add spec over calendar rows

--- spec/javascripts/models/calendar_spec.js
+++ spec/javascripts/models/calendar_spec.js
@@ -2,7 +2,7 @@ describe("Calendar", function() {
   beforeEach(function() {
     this.describedClass = Calendar;
     this.builderFactory = new Calendar.DayBuilder.Factory;
-    this.payments = [{ id: 1 }]
+    this.payments = [{ id: 1 }];
     this.buildJsonCalendar = function() {
       return {
         firstDate: 1325372400000,
@@ -10,7 +10,7 @@ describe("Calendar", function() {
         payments: this.payments
       };
     };
-    
+
     this.buildSubject = function() {
       return this.subject = new this.describedClass (
         this.buildJsonCalendar(),
@@ -40,6 +40,101 @@ describe("Calendar", function() {
       this.buildSubject();
 
       expect(this.subject.payments).toEqual(this.payments);
+    });
+  });
+
+  describe('#rows', function() {
+    describe('when the menth is the beggining of the month is the beginning of the week', function() {
+      describe('the first date', function() {
+        it('has 5 weeks', function() {
+          this.buildSubject();
+
+          expect(this.subject.rows.length).toEqual(5);
+        });
+
+        it('is the beggining of the month', function() {
+          this.buildSubject();
+
+          expect(this.subject.rows[0][0]).toEqual({ day: 1 });
+        });
+      });
+
+      describe('the last date', function() {
+        it('is the end of the week after the end of the month', function() {
+          this.buildSubject();
+
+          expect(this.subject.rows[4][6]).toEqual({ day: 4 });
+        });
+      });
+    });
+
+    describe('when the beggining of the month is in the middle of the week', function(){
+      beforeEach(function() {
+        this.buildJsonCalendar = function() {
+          return {
+            firstDate: 1328054400000,
+            lastDate:  1330473600000,
+            payments: this.payments
+          };
+        };
+      });
+
+      describe('the first date', function() {
+        it('has 5 weeks', function() {
+          this.buildSubject();
+
+          expect(this.subject.rows.length).toEqual(5);
+        });
+
+        it('is within the last month', function() {
+          console.info(this.buildJsonCalendar().lastDate);
+          this.buildSubject();
+
+          expect(this.subject.rows[0][0]).toEqual({ day: 29 });
+        });
+      });
+      describe('the last date', function() {
+        it('is the end of the week after the end of the month', function() {
+          this.buildSubject();
+
+          expect(this.subject.rows[4][6]).toEqual({ day: 3 });
+        });
+      });
+    });
+
+    describe('when the end of the month matches the end of the week', function(){
+      beforeEach(function() {
+        this.buildJsonCalendar = function() {
+          return {
+            firstDate: 1330560000000,
+            lastDate:  1333152000000,
+            payments: this.payments
+          };
+        };
+      });
+
+      describe('the first date', function() {
+        it('has 5 weeks', function() {
+          this.buildSubject();
+
+          expect(this.subject.rows.length).toEqual(5);
+        });
+
+        it('is within the last month', function() {
+          console.info(this.buildJsonCalendar().lastDate);
+          this.buildSubject();
+
+          expect(this.subject.rows[0][0]).toEqual({ day: 26 });
+        });
+      });
+
+      describe('the last date', function() {
+        it('is the end of the month', function() {
+          this.buildSubject();
+
+          expect(this.subject.rows[4][6]).toEqual({ day: 31 });
+        });
+      });
     });
   });
 });
